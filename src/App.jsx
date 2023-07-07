b1356cd7d9d4672160586815ca9a1f314af35cb1
Athlmohn <athulmnair01@gmail.com>
2023-07-08 02:45:31 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import React,{useState} from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer,ChatContainer,MessageList,Message,MessageInput,TypingIndicator } from '@chatscope/chat-ui-kit-react';
+
+const API_KEY = "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";
+
+function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am Aske!",
+      sender:"ChatGPT"
+    }
+  ]);
+
+  const handleSend = async(message)=> {
+     const newMessage = {
+      message: message,
+      sender:"user",
+      direction:"outgoing"
+     }
+     
+     const newMessages = [...messages,newMessage]
+
+     setMessages(newMessages);
+
+     setTyping(true);
+
+
+     await processMessageToChatGPT(newMessages);
+  }
+
+  async function processMessageToChatGPT(ChatMessages) {
+
+   let apiMessages = ChatMessages.map((messageObject) => {
+
+    let role = "";
+    if(messageObject.sender === "ChatGPT") {
+        role = "assistant"
+    } else {
+      role = "user"
+    }
+
+    return {role: role, content: messageObject.message};
+   });
+
+   const systemMessage = {
+    role: "system",
+    content: "Explain all concepts like i am 8 years old child"
+   }
+
+   const apiRequestBody = {
+    "model" : "gpt-3.5-turbo",
+    "messages" : [systemMessage,...apiMessages]
+   }
+
+   await fetch ("https://api.openai.com/v1/chat/completions",{
+     method : "POST",
+     headers:{
+      "Authorization": "Bearer " + API_KEY,
+      "Content-Type" : "application/json"
+     },
+     body : JSON.stringify(apiRequestBody)
+   }).then((data)=>{
+    return data.json();
+   }).then((data) =>{
+     console.log(data);
+     console.log(data.choices[0].message.content);
+     setMessages([...ChatMessages,{
+      message: data.choices[0].message.content,
+            sender: "ChatGPT"
+     }]);
+     setTyping(false);
+   });
+  }
+  return (
+    <div className='App'>
+      <h2>Unleash Conversational Intelligence</h2>
+      <div style={{position:"relative",width:"600px",height:"600px",}}>
+      <MainContainer style={{padding:"20px",borderRadius:"10px"}}>
+        <ChatContainer>
+          <MessageList scrollBehavior='smooth' typingIndicator={typing && <TypingIndicator content="Aske is Responding"/>}>
+            {messages.map((message,i) => (
+                <Message key={i} model={message}/>
+            ))}
+          </MessageList>
+          <MessageInput placeholder='Type your Message here...' onSend={handleSend}/>
+        </ChatContainer>
+      </MainContainer>
+      </div>    
+    </div>
+  )
+}
+
+export default App
